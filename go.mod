37d5d7ca1a97d4390f9dd6e42cf7cc043a3a6472
tradeguruboffin-commits <tradeguruboffin@gmail.com>
2026-02-06 01:15:50 +0000

    Initial commit: Added all script

---
+++ go.mod
@@ -0,0 +1,3 @@
+module flutter-tool
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+)
+
+//go:embed flutter-tool
+var scriptContent string
+
+var version = "1.0.1"
+
+func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
+		fmt.Printf("flutter-tool (community-distro) v%s (%s/%s)\n", version, runtime.GOOS, runtime.GOARCH)
+		os.Exit(0)
+	}
+
+	tmpDir, err := os.MkdirTemp("", "flutter-tool-*")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	scriptPath := filepath.Join(tmpDir, "flutter-tool.sh")
+	err = os.WriteFile(scriptPath, []byte(scriptContent), 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	cmd := exec.Command("/bin/bash", append([]string{scriptPath}, os.Args[1:]...)...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Env = os.Environ()
+
+	err = cmd.Run()
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			os.Exit(exitErr.ExitCode())
+		}
+		os.Exit(1)
+	}
+}
